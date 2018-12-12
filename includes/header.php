91bc3e0343142b17acb2c2e87fd5cf10585e2e79
Lars van Boven <larsvboven@hotmail.com>
2018-12-12 13:56:12 +0100

    Initial commit
    first setup commit

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="row header">
+        <div class="full-row">
+            <div class="blocks-container">
+                <div class="logo link">
+                    <h1>Beautysalon AnneFleur</h1>
+                </div>
+                <div class="media">
+                   <ul>
+                       <li class="social-media"><a href="https://instagram.com" class="link" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                       <li class="social-media"><a href="https://facebook.com" class="link" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                   </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</header>
