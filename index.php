d16fcb604fc8bd3a32d947c86a03d4452952a931
Lars van Boven <larsvboven@hotmail.com>
2018-12-17 14:34:01 +0100

    styling & links
    styling in de header toegevoegd en een appart bestand gemaakt waarin alle links van de head komen te staan

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
-<html>
+<!doctype html>
+<html lang="nl">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include('./includes/links.php'); ?>
     <title>Reserveringssysteem</title>
-    <meta charset="utf-8"/>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="./styling/header.css"/>
-    <link rel="stylesheet" type="text/css" href="./styling/assets.css"/>
 </head>
-    <body>
-        <?php include('./includes/header.php'); ?>
-
-    </body>
+<body>
+    <?php include('./includes/header.php'); ?>
+</body>
 </html>
