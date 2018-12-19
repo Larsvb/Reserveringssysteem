6a01a7c8cdcec2a852a9ecccb2531331a2f76e23
Lars van Boven <larsvboven@hotmail.com>
2018-12-19 10:55:54 +0100

    slider & css
    Afbeeldingen toegevoegd, styling toegepast voor de slider en de intro tekst en een algemeen bestand gemaakt voor links die in de head komen te staan

--- includes/links.php
+++ includes/links.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="./styling/header.css"/>
 <link rel="stylesheet" type="text/css" href="./styling/assets.css"/>
+<link rel="stylesheet" type="text/css" href="./styling/style.css"/>
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800|Raleway:400,600,700,800" rel="stylesheet">
--- index.php
+++ index.php
@@ -10,5 +10,81 @@
 </head>
 <body>
     <?php include('./includes/header.php'); ?>
+    <div class="row">
+        <div class="full-row">
+            <div class="blocks-container slider">
+                <div class="slider-content">
+                    <div class="block image">
+                        <img src="./images/pf.jpg">
+                    </div>
+                    <div class="text">
+                        <div class="block title">
+                            <h2>WELKOM.</h2>
+                        </div>
+                        <div class="block text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                                vulputate eget, arcu. In enim justo,<br>
+                                rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante,
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row appointment-info">
+        <div class="full-row">
+            <div class="blocks-container">
+                <div class="block title">
+                    <h3>Afspraakgegevens</h3>
+                </div>
+                <form>
+                    <div>
+                        <label>Behandeling</label>
+                        <select name="cars">
+                            <option value="Basis gezichtsbehandeling ">Basis gezichtsbehandeling 20,00</option>
+                            <option value="">Deluxe gezichtsbehandeling 30,00</option>
+                            <option value="">Microdermabrasie 35,00</option>
+                            <option value="">Acne behandeling 30,00</option>
+                            <option value="">Jeugd gezichtsbehandeling 25,00</option>
+                            <option value="">One by one wimperextensions 45,00</option>
+                            <option value="">Opvullen one by one wimperextensions 2 weken = 20 | 3 weken 30,00</option>
+                            <option value="">Plukjes wimperextensions 22,50</option>
+                            <option value="">Wimperlifting 32,50 + verf = 40,00</option>
+                            <option value="">Wimpers verven 7,50</option>
+                            <option value="">Wenkbrauwen harsen/epileren 7,50</option>
+                            <option value="">Gezicht harsen (verschillende delen dus kin, bovenlip etc) 22,50</option>
+                            <option value="">Lichaam harsen (oksels, benen, armen) vanaf 12,50 tot 22,50</option>
+                            <option value="">Gellak op de natuurlijke nagels handen en voeten 22,50</option>
+                            <option value="">Pedicure 22,50</option>
+                            <option value="">Manicure 22,50</option>
+                            <option value="">Nieuwe set acryl 45,00</option>
+                            <option value="">Opvullen acryl 35,00</option>
+                            <option value="">Massages (rug, benen, armen) 20,00</option>
+                            <option value="">Visagie behandeling vanaf 30,00</option>
+                            <option value="">Bruids make-up vanaf 30,00</option>
+                            <option value="">Henna wenkbrauw behandeling ~25,00</option>
+
+
+                        </select>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="row personal-info">
+        <div class="full-row">
+            <div class="blocks-container">
+
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="full-row">
+            <div class="blocks-container">
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
